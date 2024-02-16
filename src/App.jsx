4097b893f6ae1270493606a114fec540f6ceb7e1
Alles <alleskaput43@gmail.com>
2024-02-16 23:18:31 +0300

    Настроил иконки, вынес хедер в отдельный файл

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header.jsx";
 
 const user = {
-  firstName: "Иван",
-  lastName: "Иванов",
+    firstName: "Иван",
+    lastName: "Иванов",
 };
 
 function formateUser(user) {
-  return user.firstName + " " + user.lastName;
+    return user.firstName + " " + user.lastName;
 }
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return <div>Hello {formateUser(user)}</div>;
+    return (
+        <div>
+            <Header />
+            <main>
+                <div>Hello {formateUser(user)}</div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
